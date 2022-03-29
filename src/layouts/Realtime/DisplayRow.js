a90f9b6cdbd962bf5cd2b36c3e9870981edf15ea
kyoungd <kyoungd@hotmail.com>
2022-03-29 16:59:21 -0700

    added more vsa options

--- src/layouts/Realtime/DisplayRow.js
+++ src/layouts/Realtime/DisplayRow.js
@@ -6,6 +6,10 @@ export default function UserRow(row) {
       <TableCell align="left">${row.price}</TableCell>
       <TableCell align="left">{row.vsa === 1 ? 'yes' : 'no'}</TableCell>
       <TableCell align="left">{row.vsa === 2 ? 'yes' : 'no'}</TableCell>
+      <TableCell align="left">{row.vsa === 3 || row.vsa === 4 ? 'yes' : 'no'}</TableCell>
+      <TableCell align="left">
+        {row.vsa === 5 || row.vsa === 6 || row.vsa === 7 ? 'yes' : 'no'}
+      </TableCell>
       <TableCell align="left">{row.vsa === 10 ? 'yes' : 'no'}</TableCell>
     </>
   );
