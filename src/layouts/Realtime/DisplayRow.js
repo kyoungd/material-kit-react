580ebc793f5135249618b0224e6f99496da4930f
kyoungd <kyoungd@hotmail.com>
2022-04-12 23:35:13 -0700

    text changes

--- src/layouts/Realtime/DisplayRow.js
+++ src/layouts/Realtime/DisplayRow.js
@@ -6,11 +6,8 @@ export default function UserRow(row) {
       <TableCell align="left">${row.price}</TableCell>
       <TableCell align="left">{row.vsa === 1 ? 'yes' : 'no'}</TableCell>
       <TableCell align="left">{row.vsa === 2 ? 'yes' : 'no'}</TableCell>
-      <TableCell align="left">{row.vsa === 3 || row.vsa === 4 ? 'yes' : 'no'}</TableCell>
-      <TableCell align="left">
-        {row.vsa === 5 || row.vsa === 6 || row.vsa === 7 ? 'yes' : 'no'}
-      </TableCell>
-      <TableCell align="left">{row.vsa === 10 ? 'yes' : 'no'}</TableCell>
+      <TableCell align="left">{row.sd ? 'yes' : 'no'}</TableCell>
+      <TableCell align="left">{row.ab ? 'yes' : 'no'}</TableCell>
       <TableCell align="left">{row.st || row.ef ? 'yes' : 'no'}</TableCell>
     </>
   );
