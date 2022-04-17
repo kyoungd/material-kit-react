989619fa59d87d553663107ebd07bc582891cbfd
kyoungd <kyoungd@hotmail.com>
2022-04-16 22:29:54 -0700

    standarized cs

--- src/layouts/Realtime/DisplayRow.js
+++ src/layouts/Realtime/DisplayRow.js
@@ -1,14 +1,17 @@
+/* eslint-disable no-bitwise */
 import { TableCell } from '@mui/material';
 
 export default function UserRow(row) {
   return (
     <>
       <TableCell align="left">${row.price}</TableCell>
-      <TableCell align="left">{row.vsa === 1 ? 'yes' : 'no'}</TableCell>
-      <TableCell align="left">{row.vsa === 2 ? 'yes' : 'no'}</TableCell>
-      <TableCell align="left">{row.sd ? 'yes' : 'no'}</TableCell>
-      <TableCell align="left">{row.ab ? 'yes' : 'no'}</TableCell>
-      <TableCell align="left">{row.cs > 0 ? 'yes' : 'no'}</TableCell>
+      <TableCell align="center">{(row.vsa & 1) === 1 ? 'yes' : 'no'}</TableCell>
+      <TableCell align="center">{(row.vsa & 2) === 2 ? 'yes' : 'no'}</TableCell>
+      <TableCell align="center">{(row.cs & 1) === 1 ? 'yes' : 'no'}</TableCell>
+      <TableCell align="center">{(row.cs & 2) === 2 ? 'yes' : 'no'}</TableCell>
+      <TableCell align="center">{(row.cs & 4) === 4 ? 'yes' : 'no'}</TableCell>
+      <TableCell align="center">{(row.cs & 8) === 8 ? 'yes' : 'no'}</TableCell>
+      <TableCell align="center">{(row.cs & 16) === 16 ? 'yes' : 'no'}</TableCell>
     </>
   );
 }
