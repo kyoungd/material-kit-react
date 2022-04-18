6dee9db51b83691453ab25c604926af7a3c0cfe1
kyoungd <kyoungd@hotmail.com>
2022-04-18 07:17:42 -0700

    price format

--- src/layouts/Realtime/DisplayRow.js
+++ src/layouts/Realtime/DisplayRow.js
@@ -4,7 +4,7 @@ import { TableCell } from '@mui/material';
 export default function UserRow(row) {
   return (
     <>
-      <TableCell align="left">${row.price}</TableCell>
+      <TableCell align="right">${row.price}</TableCell>
       <TableCell align="center">{(row.vsa & 1) === 1 ? 'yes' : 'no'}</TableCell>
       <TableCell align="center">{(row.vsa & 2) === 2 ? 'yes' : 'no'}</TableCell>
       <TableCell align="center">{(row.cs & 1) === 1 ? 'yes' : 'no'}</TableCell>
