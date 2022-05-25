d63a8fa955c034e6039ebd77c5fc3bd35609bf5d
kyoungd <kyoungd@hotmail.com>
2022-05-25 14:33:26 -0700

    more clean up on intraday trading page

--- src/layouts/Realtime/DisplayRow.js
+++ src/layouts/Realtime/DisplayRow.js
@@ -5,14 +5,11 @@ export default function UserRow(row) {
   let candle = '';
   candle += row.vsa === 1 ? ' thrust' : '';
   candle += row.vsa === 2 ? ' climax' : '';
-  candle += (row.cs & 1) === 1 ? ' engulf' : '';
-  candle += (row.cs & 2) === 2 ? ' star' : '';
   return (
     <>
       <TableCell align="right">${row.price.toFixed(2)}</TableCell>
       <TableCell align="center">{candle}</TableCell>
       <TableCell align="center">{(row.cs & 4) === 4 ? 'yes' : ''}</TableCell>
-      <TableCell align="center">{(row.cs & 8) === 8 ? 'yes' : ''}</TableCell>
       <TableCell align="center">{(row.cs & 16) === 16 ? 'yes' : ''}</TableCell>
       <TableCell align="center">{row.tf}</TableCell>
     </>
